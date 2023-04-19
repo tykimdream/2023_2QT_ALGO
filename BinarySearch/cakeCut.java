5d0b8512c265e1a8db71a43a28867b469a55f615
김지환 <tykimdream@naver.com>
2023-04-19 09:19:36 +0900

    binary search

---
+++ BinarySearch/cakeCut.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class cakeCut {
+    static int n, m, l;
+    static int[] toCut;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        toCut = new int[m + 1];
+
+        for(int i =0; i <m;i++){
+            st = new StringTokenizer(br.readLine());
+            toCut[i] = Integer.parseInt(st.nextToken());
+        }
+        toCut[m] = l;
+
+        for(int i =0; i <n;i++){
+            int answer = 0;
+            int q = Integer.parseInt(br.readLine());
+            int low = 0;
+            int high = l;
+            while(low <= high){
+                int mid = (high+low)/2;
+                if(possible(mid, q)){
+                    answer = Math.max(answer, mid);
+                    low = mid + 1;
+                }else {
+                    high = mid-1;
+                }
+            }
+            System.out.println(answer);
+        }
+        return;
+    }
+
+    public static boolean possible(int mid, int q){
+        int cutCnt = 0;
+        int prev = 0;
+        for(int i =0; i <= m; i++){
+            if(toCut[i] - prev >= mid){
+                prev = toCut[i];
+                q--;
+            }
+        }
+
+        // 지금 크기로 잘랐는데 q를 만족하지 못하는 경우 크기를 더 줄여봐야한다.
+        return q < 0? true : false;
+    }
+}
---
+++ BinarySearch/기타레슨.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 기타레슨 {
+    static int n, m;
+    static int[] list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int low = 0;
+        int high = 0;
+
+        list = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+            low = Math.max(low, list[i]);
+            high += list[i];
+        }
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int cnt = 0;
+            int sum = 0;
+            for(int i = 0; i < n; i++){
+                if(sum + list[i] > mid) {
+                    sum = 0;
+                    cnt++;
+                }
+                sum += list[i];
+            }
+            if(sum != 0) cnt++;
+            if(cnt > m){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        System.out.println(low);
+    }
+}
