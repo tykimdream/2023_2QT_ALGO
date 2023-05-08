77efa7be3ff7fd137be1e1d46201ffae90b8c84a
김지환 <tykimdream@naver.com>
2023-05-09 08:45:06 +0900

    문자열돌리기

---
+++ 프로그래머스/문자열돌리기.java
@@ -0,0 +1,14 @@
+package 프로그래머스;
+
+import java.util.Scanner;
+
+public class 문자열돌리기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i =0; i < a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+
+}
