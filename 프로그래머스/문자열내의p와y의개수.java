00be5c9fc52da47dffe8c55a62c10f569e57c820
김지환 <tykimdream@naver.com>
2023-05-16 09:33:54 +0900

    stream method filter

---
+++ 프로그래머스/문자열내의p와y의개수.java
@@ -0,0 +1,9 @@
+package 프로그래머스;
+
+public class 문자열내의p와y의개수 {
+    boolean solution(String s) {
+        s = s.toUpperCase();
+
+        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+    }
+}
